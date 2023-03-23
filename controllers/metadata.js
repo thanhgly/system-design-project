a07efdbd35257acb872b5f7dd73accee700755d5
Thanh Ly <thanhgly.se@gmail.com>
2023-03-22 19:51:41 -0700

    removed a redundant if statement in metadata controller

--- controllers/metadata.js
+++ controllers/metadata.js
@@ -4,11 +4,6 @@ module.exports = {
   get: (req, res) => {
     let { product_id } = req.query;
 
-    if (isNaN(product_id)) {
-      res.status(422).send('Error: invalid product_id provided');
-      return;
-    }
-
     metadata.get(product_id)
     .then(response => res.json(response))
     .catch(err => {
